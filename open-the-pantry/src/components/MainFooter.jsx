1c7070634c1f643e2f7315196564544eae79eff5
irenaliu <irena@ualberta.ca>
2020-12-18 03:35:52 -0700

    Update MainFooter.jsx

--- open-the-pantry/src/components/MainFooter.jsx
+++ open-the-pantry/src/components/MainFooter.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { MDBCol, MDBContainer, MDBRow, MDBFooter } from 'mdbreact';
+import { MDBCol, MDBContainer, MDBRow, MDBFooter, MDBIcon } from 'mdbreact';
 
 const MainFooter = () => {
   return (
     <MDBFooter color='deep-orange lighten-4' className='font-small pt-4 mt-4'>
       <MDBContainer fluid className='text-center text-md-left'>
         <MDBRow>
-          <MDBCol md='6'>
+          <MDBCol md='3'>
             <h5 className='title'>Footer Content</h5>
             <p>
               Here you can use rows and columns here to organize your footer
@@ -14,27 +14,23 @@ const MainFooter = () => {
             </p>
           </MDBCol>
           <MDBCol md='6'>
-            <h5 className='title'>Links</h5>
-            <ul>
-              <li className='list-unstyled'>
-                <a href='#!'>Link 1</a>
-              </li>
-              <li className='list-unstyled'>
-                <a href='#!'>Link 2</a>
-              </li>
-              <li className='list-unstyled'>
-                <a href='#!'>Link 3</a>
-              </li>
-              <li className='list-unstyled'>
-                <a href='#!'>Link 4</a>
+            <h5 className='title text-center'>Connect With Me</h5>
+            <ul className='pl-0'>
+              <li className='list-unstyled text-center'>
+                <a href='https://www.instagram.com/openthepantry/'>
+                  <MDBIcon fab icon='instagram' />
+                </a>
               </li>
             </ul>
+          </MDBCol>
+          <MDBCol md='3'>
+            <h5 className='title text-center'>Get In Touch</h5>
           </MDBCol>
         </MDBRow>
       </MDBContainer>
       <div className='footer-copyright text-center py-3'>
         <MDBContainer fluid>
-          &copy; {new Date().getFullYear()} Copyright: Irena Liu
+          &copy; {new Date().getFullYear()} Sirena Kim
         </MDBContainer>
       </div>
     </MDBFooter>
