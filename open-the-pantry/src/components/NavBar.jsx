2cc69e305383f127d4cd18b3651788f1492546c1
irenaliu <irena@ualberta.ca>
2020-12-18 03:17:12 -0700

    Update navbar component

--- open-the-pantry/src/components/NavBar.jsx
+++ open-the-pantry/src/components/NavBar.jsx
@@ -1,68 +1,67 @@
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem } from 'mdbreact';
 
-import Home from '../pages/Home.jsx';
-import SearchResults from '../pages/SearchResults.jsx';
-import Categories from '../pages/Categories.jsx';
-import Contact from '../pages/Contact.jsx';
-import About from '../pages/About.jsx';
-import Recipe from '../pages/Recipe.jsx';
+import Home from '../pages/Home';
+import SearchResults from '../pages/SearchResults';
+import Categories from '../pages/Categories';
+import Contact from '../pages/Contact';
+import About from '../pages/About';
+import Recipe from '../pages/Recipe';
 
 const NavBar = () => {
   return (
     <Router>
       {/* Navigation */}
-      <MDBNavbar expand='md'>
+      <MDBNavbar color='deep-orange lighten-4' expand='md'>
         <MDBNavbarBrand>
           <Link to='/'>Open The Pantry</Link>
         </MDBNavbarBrand>
         <MDBNavbarNav right>
           <MDBNavItem active>
             <Link className='link' to='/categories'>
-              CATEGORIES
+              Categories
             </Link>
           </MDBNavItem>
           <MDBNavItem>
             <Link className='link' to='/about'>
-              ABOUT
+              About
             </Link>
           </MDBNavItem>
           <MDBNavItem>
             <Link className='link' to='/recipe'>
-              RECIPE
+              Recipe
             </Link>
           </MDBNavItem>
           <MDBNavItem>
             <Link className='link' to='/contact'>
-              CONTACT
+              Contact
             </Link>
           </MDBNavItem>
         </MDBNavbarNav>
       </MDBNavbar>
 
       {/* Routing for Pages */}
-      <div className='container'>
-        <Switch>
-          <Route exact path='/'>
-            <Home />
-          </Route>
-          <Route path='/categories'>
-            <Categories />
-          </Route>
-          <Route path='/about'>
-            <About />
-          </Route>
-          <Route path='/recipes'>
-            <Recipe />
-          </Route>
-          <Route path='/contact'>
-            <Contact />
-          </Route>
-          <Route path='/search'>
-            <SearchResults />
-          </Route>
-        </Switch>
-      </div>
+
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/categories'>
+          <Categories />
+        </Route>
+        <Route path='/about'>
+          <About />
+        </Route>
+        <Route path='/recipe'>
+          <Recipe />
+        </Route>
+        <Route path='/contact'>
+          <Contact />
+        </Route>
+        <Route path='/search'>
+          <SearchResults />
+        </Route>
+      </Switch>
     </Router>
   );
 };
