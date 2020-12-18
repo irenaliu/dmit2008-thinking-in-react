bc109121f7115a229341882a131a11542eba7264
irenaliu <irena@ualberta.ca>
2020-12-18 03:17:39 -0700

    Create MainHeader component

---
+++ open-the-pantry/src/components/MainHeader.jsx
@@ -0,0 +1,13 @@
+const MainHeader = () => {
+  return (
+    <div className='cover-img-container'>
+      <img
+        className='cover-img'
+        src='https://images.unsplash.com/photo-1522336849566-02cbb8bffc4c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1951&q=80'
+        alt='Baked good'
+      />
+    </div>
+  );
+};
+
+export default MainHeader;
