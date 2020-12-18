40c80e4a18e00e9a18114ff3f51312ff83416154
irenaliu <irena@ualberta.ca>
2020-12-18 03:18:08 -0700

    Create BlogIntro component

---
+++ open-the-pantry/src/components/BlogIntro.jsx
@@ -0,0 +1,25 @@
+const BlogIntro = () => {
+  return (
+    <div className='d-flex my-5'>
+      <div className='col-6 blog-intro-img-container'>
+        <img
+          className='blog-intro-img'
+          src='https://images.squarespace-cdn.com/content/v1/5f1d18f813fdc32b339945a1/1595754128520-Y23IPJ6HHCSOUYZ48ADM/ke17ZwdGBToddI8pDm48kCMflZsGmPbOje7wmTQP_bJ7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0pmy3nA_zcH46jcY3zQ1h8j9_oOwOJRBDqTCsKewmoMBj8SdD3IjF7MRjL_6nFoulQ/2020-03-13+07.00.19+2.jpg?format=500w'
+          alt='Sirena smiling holding her child'
+        />
+      </div>
+      <div className='col-6'>
+        <h1>My Name is Sirena!</h1>
+        <p>Wondering what you can make with the ingredients in your pantry?</p>
+        <p>
+          I am Sirena and welcome to Open the Pantry. I am an ordinary mum who
+          loves to bake and cook in a beautiful country called New Zealand. I
+          hope to share with you all aesthetic recipes from my kitchen!
+        </p>
+        <p>Let’s Open your Pantry!</p>
+      </div>
+    </div>
+  );
+};
+
+export default BlogIntro;
